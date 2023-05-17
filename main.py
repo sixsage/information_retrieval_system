c04647b613e2b35d638e5ae77424626d650bfd32
sixsage <sixsage02@gmail.com>
2023-05-17 09:18:34 -0700

    add check if json files exist, else build them

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ def load_json(file):
         return json.loads(x)
 
 if __name__ == "__main__":
+    if not (os.path.exists("inverted_index.json") and os.path.exists("urlindex.json")):
+        buildindex()
     user_input = input("SEARCH: ")
     user_input = user_input.split()
     stemmer = PorterStemmer()
     user_input = [stemmer.stem(token) for token in user_input]
     iid = load_json("inverted_index.json")
-    #urls = load_json("urlindex.json")
+    urls = load_json("urlindex.json")
     target_doc_ids = query(user_input, iid)
     for doc_id in target_doc_ids:
         print(doc_id)
