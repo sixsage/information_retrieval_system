1fa5de979ec4ae3c92c1b435ac5e267003709cfc
Jacob Lee <jacoblee0001@gmail.com>
2023-05-16 18:33:28 -0700

    fixed bug in query

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def load_json(file):
 if __name__ == "__main__":
     user_input = input("SEARCH: ")
     user_input = user_input.split()
+    stemmer = PorterStemmer()
+    user_input = [stemmer.stem(token) for token in user_input]
     iid = load_json("inverted_index.json")
-    urls = load_json("urlindex.json")
+    #urls = load_json("urlindex.json")
     targetDocIds = query(user_input, iid)
     for docid in targetDocIds:
-        print(urls[docid])
+        print(docid)
+        #print(urls[docid])
 
--- search.py
+++ search.py
@@ -7,17 +7,17 @@ def query(terms: list[str], iid: dict[str, list[int]]) -> list[int]:
             docs = iid[term]
         else:
             newdocs = iid[term]
-            i = 0 # index for docs
+            i = 0 # index for old docs
             u = 0 # index for new term docs
             new = [] # resulting intersection of docs
             while i < len(docs) and u < len(newdocs):
-                if docs[i] == newdocs[u]:
+                if docs[i][0] == newdocs[u][0]:
                     new.append(docs[i])
                     i += 1
                     u += 1
-                elif docs[i] < newdocs[u]:
+                elif docs[i][0] < newdocs[u][0]:
                     i += 1
                 else:
                     u += 1
             docs = new
-    return docs
+    return [x[0] for x in docs]
