4e422bfdf530213195eb8b3ad341e624423175ff
Jacob Lee <jacoblee0001@gmail.com>
2023-06-01 13:42:39 -0700

    add back heading_iid to query_processin

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     # MULTIPROCESSING IN PROGRESS
     # make the query_iid in query_processing? passing in the index objects for now
     q = multiprocessing.Queue()
-    processbigrams = multiprocessing.Process(target=search.query_processing, args=(q, search.bigramify_query(user_input), bigrams, TOTAL_PAGES))
+    processbigrams = multiprocessing.Process(target=search.query_processing, args=(q, search.bigramify_query(user_input), bigrams, TOTAL_PAGES, headings_iid))
 
     # processtrigrams = multiprocessing.Process(target=search.query_processing, args=[search.trigramify_query(user_input), trigrams.index, TOTAL_PAGES])
 
